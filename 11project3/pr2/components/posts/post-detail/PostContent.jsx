c7236a126cb4b89a4e7ff313a0163efb541411e2
Artem Klishch <artemklishch@gmail.com>
2022-02-13 23:32:38 +0200

    dfsgsdfg

--- 11project3/pr2/components/posts/post-detail/PostContent.jsx
+++ 11project3/pr2/components/posts/post-detail/PostContent.jsx
@@ -1,14 +1,27 @@
 import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./PostHeader";
+import Image from 'next/image'
 
 const PostContent = (props) => {
   const { title, slug, image, content } = props.post
   const imagePath = `/images/posts/${slug}/${image}`;
+  const customRenderers = {
+    img(image) {
+      return (
+        <Image
+          src={`/images/posts/${slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+          priority={true}
+        />)
+    }
+  }
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 };
